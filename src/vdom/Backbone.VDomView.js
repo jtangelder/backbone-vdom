47292cfe01c4f6327d2d70f914fc4b9c4c82c5c8
Jorik Tangelder <jtangelder@gmail.com>
2014-12-26 23:40:00 +0100

    refactor

--- src/vdom/Backbone.VDomView.js
+++ src/vdom/Backbone.VDomView.js
@@ -12,6 +12,8 @@ var createElement = require('virtual-dom/create-element');
 var h = require('virtual-dom/h');
 
 var VDomView = Backbone.View.extend({
+	_vDom: {},
+
 	/**
 	 * create a virtual-dom structure
 	 * receives the view instance as an argument
@@ -24,23 +26,23 @@ var VDomView = Backbone.View.extend({
 	/**
 	 * trigger the rendering of the virtualDom
 	 * default the raf strategy is being used
-	 * @param {Boolean} [force=false]
+	 * @param {Boolean} [immediately=false]
 	 * @returns {VDomView}
 	 */
-	render: function (force) {
+	render: function (immediately) {
 		// initial render
-		if (!this.vDomElement) {
-			this.vDomRender();
-			this.vDomElement = createElement(this._tree);
-			this.el.appendChild(this.vDomElement);
+		if (!this._vDom.element) {
+			this._vDomRender();
+			this._vDom.element = createElement(this._vDom.tree);
+			this.el.appendChild(this._vDom.element);
 		}
 		// immediately render the virtualDom
-		else if(force === true) {
-			this.vDomRender();
+		else if(immediately === true) {
+			this._vDomRender();
 		}
 		// queue the render to the next animation frame
 		else {
-			this.vDomQueueRender();
+			this._vDomQueueRender();
 		}
 
 		return this;
@@ -49,30 +51,30 @@ var VDomView = Backbone.View.extend({
 	/**
 	 * do a virtual dom diff and update the real DOM
 	 */
-	vDomRender: function() {
+	_vDomRender: function() {
 		var newTree = this.template(this);
 
-		if (this._tree && this.vDomElement) {
-			this._patches = diff(this._tree, newTree);
-			this.vDomElement = patch(this.vDomElement, this._patches);
+		if (this._vDom.tree && this._vDom.element) {
+			this._vDom.patches = diff(this._vDom.tree, newTree);
+			this._vDom.element = patch(this._vDom.element, this._vDom.patches);
 		}
 
-		this._tree = newTree;
+		this._vDom.tree = newTree;
 	},
 
 	/**
 	 * wait for the next animation frame to update the dom
 	 */
-	vDomQueueRender: function() {
-		if(this._rafScheduled) {
+	_vDomQueueRender: function() {
+		if(this._vDom.rafScheduled) {
 			return;
 		}
 
-		this._rafScheduled = true;
+		this._vDom.rafScheduled = true;
 
 		raf(function() {
-			this._rafScheduled = false;
-			this.vDomRender()
+			this._vDom.rafScheduled = false;
+			this._vDomRender()
 		}.bind(this));
 	}
 });
@@ -81,11 +83,15 @@ Backbone.VDomView = VDomView;
 
 // expose the hyperscript function to create a virtual dom tree
 Backbone.VDomView.h = function(tag, attrs, childs) {
+	// known html tag, pass to hyperscript
 	if(typeof tag === 'string') {
 		return h(tag, attrs, childs);
-	} else if(tag instanceof Node) {
+	}
+	// create an html element widget
+	if(tag instanceof Node) {
 		return new ElementWidget(tag, attrs, (attrs && attrs.key));
 	}
+	// create a backbone view widget
 	return new ViewWidget(tag, attrs, (attrs && attrs.key));
 };
 
