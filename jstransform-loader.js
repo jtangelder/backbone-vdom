f9f84a8eadd8146b72034fe65bbc59f4bd7a49fd
Jorik Tangelder <jorik@blendle.nl>
2015-01-05 20:55:33 +0100

    only es6 and jsx visitors

--- jstransform-loader.js
+++ jstransform-loader.js
@@ -1,5 +1,4 @@
 var transform = require('jstransform').transform;
-
 var es6ArrowFunctions = require('jstransform/visitors/es6-arrow-function-visitors');
 var es6Classes = require('jstransform/visitors/es6-class-visitors');
 var es6Destructuring = require('jstransform/visitors/es6-destructuring-visitors');
@@ -7,7 +6,6 @@ var es6ObjectConciseMethod = require('jstransform/visitors/es6-object-concise-me
 var es6ObjectShortNotation = require('jstransform/visitors/es6-object-short-notation-visitors');
 var es6RestParameters = require('jstransform/visitors/es6-rest-param-visitors');
 var es6Templates = require('jstransform/visitors/es6-template-visitors');
-var es7SpreadProperty = require('jstransform/visitors/es7-spread-property-visitors');
 var jsx = require('jsx-transform/lib/visitor');
 
 var transformVisitors = [
@@ -18,7 +16,6 @@ var transformVisitors = [
     es6ObjectShortNotation.visitorList,
     es6RestParameters.visitorList,
     es6Templates.visitorList,
-    es7SpreadProperty.visitorList,
     [jsx]
 ];
 
