ee6d1c95f8d9034adde8f850a9b978a1e0dd28d7
Jorik Tangelder <jorik@blendle.nl>
2015-01-05 20:54:00 +0100

    less code

--- jstransform-loader.js
+++ jstransform-loader.js
@@ -10,37 +10,21 @@ var es6Templates = require('jstransform/visitors/es6-template-visitors');
 var es7SpreadProperty = require('jstransform/visitors/es7-spread-property-visitors');
 var jsx = require('jsx-transform/lib/visitor');
 
-var transformVisitors = {
-    'es6-arrow-functions': es6ArrowFunctions.visitorList,
-    'es6-classes': es6Classes.visitorList,
-    'es6-destructuring': es6Destructuring.visitorList,
-    'es6-object-concise-method': es6ObjectConciseMethod.visitorList,
-    'es6-object-short-notation': es6ObjectShortNotation.visitorList,
-    'es6-rest-params': es6RestParameters.visitorList,
-    'es6-templates': es6Templates.visitorList,
-    'es7-spread-property': es7SpreadProperty.visitorList,
-    'jsx': [jsx]
-};
-
-/**
- * Specifies the order in which each transform should run.
- */
-var transformRunOrder = [
-    'es6-arrow-functions',
-    'es6-object-concise-method',
-    'es6-object-short-notation',
-    'es6-classes',
-    'es6-rest-params',
-    'es6-templates',
-    'es6-destructuring',
-    'es7-spread-property',
-    'jsx'
+var transformVisitors = [
+    es6ArrowFunctions.visitorList,
+    es6Classes.visitorList,
+    es6Destructuring.visitorList,
+    es6ObjectConciseMethod.visitorList,
+    es6ObjectShortNotation.visitorList,
+    es6RestParameters.visitorList,
+    es6Templates.visitorList,
+    es7SpreadProperty.visitorList,
+    [jsx]
 ];
 
-var visitorList = [];
-for (var i = 0, il = transformRunOrder.length; i < il; i++) {
-    visitorList = visitorList.concat(transformVisitors[transformRunOrder[i]]);
-}
+var visitorList = transformVisitors.reduce(function(list, item) {
+    return list.concat(item);
+});
 
 module.exports = function (code) {
     return transform(visitorList, code, {
